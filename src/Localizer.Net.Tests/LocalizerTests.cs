0338ac14689dc49d097fac52ec7e59cc31b2b8dd
Patrick Michallet <patrick@michallet.co.uk>
2020-10-03 13:26:09 +0200

    Refactor tests to reduce repetition & allow locales to be searched without throwing.

--- src/Localizer.Net.Tests/LocalizerTests.cs
+++ src/Localizer.Net.Tests/LocalizerTests.cs
@@ -6,6 +6,10 @@ namespace Localizer.Net.Tests
 {
     public class LocalizerTests
     {
+        private readonly ILocalization _localization = new LocalizationBuilder()
+            .UseJsonFiles("locales")
+            .Build();
+
         [Test]
         public void AssertCanLoadJsonLocalisations()
         {
@@ -38,7 +42,7 @@ namespace Localizer.Net.Tests
                 .UseJsonFiles("locales")
                 .Build();
 
-            Localizer.Inject(localization, Assembly.GetAssembly(typeof(LocalizableClass)));
+            Localizer.Inject(_localization, Assembly.GetAssembly(typeof(LocalizableClass)));
 
             Assert.AreEqual("world", LocalizableClass.WorldString.Localize("en-US"));
             Assert.AreEqual("told me", LocalizableClass.SomebodyOnceString.Localize("en-US"));
@@ -47,13 +51,9 @@ namespace Localizer.Net.Tests
         [Test]
         public void AssertLocalizationCanBeInjectedInstance()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
             var instance = new LocalizableInstanceClass();
 
-            Localizer.Inject(instance, localization);
+            Localizer.Inject(instance, _localization);
 
             Assert.AreEqual("world", instance.WorldString.Localize("en-US"));
         }
@@ -61,65 +61,47 @@ namespace Localizer.Net.Tests
         [Test]
         public void AssertExecuteSimpleReplacement()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
-            var test1 = localization.Resolve("en-US", "context-tests.test1", ("drill", "test"));
+            var test1 = _localization.Resolve("en-US", "context-tests.test1", ("drill", "test"));
             Assert.AreEqual("This is not a test!", test1);
         }
 
         [Test]
         public void AssertExecuteMultipleReplacement()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
-            var test2 = localization.Resolve("en-US", "context-tests.test2", ("drill", "test"), ("drill2", "test2"));
+            var test2 = _localization.Resolve("en-US", "context-tests.test2", ("drill", "test"), ("drill2", "test2"));
             Assert.AreEqual("This is not a test test2", test2);
         }
 
         [Test]
         public void AssertExecuteStatementReplacement()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
-            var test2 = localization.Resolve("en-US", "context-tests.test3", ("isDrill", true), ("drill", "drill"));
-            Assert.AreEqual("This is not a drill!", test2);
+            var test3 = _localization.Resolve("en-US", "context-tests.test3", ("isDrill", true), ("drill", "drill"));
+            Assert.AreEqual("This is not a drill!", test3);
         }
 
         [Test]
         public void AssertExecuteStatementReplacementAtStart()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
-            var test4 = localization.Resolve("en-US", "context-tests.test4", ("isKieran", true));
+            var test4 = _localization.Resolve("en-US", "context-tests.test4", ("isKieran", true));
             Assert.AreEqual("Kieran is cool", test4);
         }
 
         [Test]
         public void AssertThrowsUnclosedBrace()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
-
-            Assert.Throws<LocalizerException>(() => localization.Resolve("en-US", "context-tests.test5"));
+            Assert.Throws<LocalizerException>(() => _localization.Resolve("en-US", "context-tests.test5"));
         }
 
         [Test]
         public void AssertThrowsEmptyScript()
         {
-            var localization = new LocalizationBuilder()
-                .UseJsonFiles("locales")
-                .Build();
+            Assert.Throws<LocalizerException>(() => _localization.Resolve("en-US", "context-tests.test6"));
+        }
 
-            Assert.Throws<LocalizerException>(() => localization.Resolve("en-US", "context-tests.test6"));
+        [Test]
+        public void AssertThrowsMissingKey()
+        {
+            Assert.Throws<LocalizerException>(() => _localization.Resolve("en-US", "I don't exist"));
         }
     }
 
